ef1975282a2738265e292037aad09a904a0b8189
Piskorski Etienne <etienne.piskorski@ensg.eu>
2020-02-24 20:18:47 +0100

    maj build + web doc + test pipeline

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,9 @@
-cd src
+# generation de l"application (cas wms)
+cd src/wms
 mvn clean install
+# generation du site web statique de documentation
+cd ../../doc/en
+mvn 
+mvn clean install
+mvn process-resources
+mvn compile
